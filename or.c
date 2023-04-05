c6133a42fa065b5d458a84aa6911c5cf04f14cc4
30114 <>
2023-04-05 01:06:45 +0000

    이진법

---
+++ or.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+  int a=15;
+  int b= 20;
+  int c = a|b;
+
+  printf("0r 연산 결과 ; %d" , c); 
+}
--- val.c
+++ val.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-int a, b; //쓰레기 값으로 초기화
-int c=30, d=40;
-a=10;
-b=20;
-printf("%d %d \n", a, b);
-printf(" %d %d \n", c, d);
-return 0;
-}
+// #include <stdio.h>
+// int a, b; //쓰레기 값으로 초기화
+// int c=30, d=40;
+// a=10;
+// b=20;
+// printf("%d %d \n", a, b);
+// printf(" %d %d \n", c, d);
+// return 0;
+// }
